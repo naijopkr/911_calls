6aa9c159399162cac8377447b7d1130ba71033a9
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-05-13 13:48:42 -0500

    set dataframe up

--- __main__.py
+++ __main__.py
@@ -1,2 +1,23 @@
-if __name__ == '__main__':
-    print('Hello world!')
+import numpy as np
+import pandas as pd
+import matplotlib as plt
+import plotly.graph_objects as go
+import plotly.io as pio
+
+df = pd.read_csv('911.csv', dtype=dict(
+    lat = float,
+    lng = float,
+    desc = str,
+    zip = str,
+    title = str,
+    timeStamp = str,
+    twp = str,
+    addr = str,
+    e = str
+))
+
+print('911 CALLS INFO')
+print(df.info())
+
+print('911 CALLS HEAD')
+print(df.head())
