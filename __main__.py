f753dabbefc8e075916bf20a65fd561cf1b55e1a
Ariel Barcellos <barcellos.ariel@gmail.com>
2020-05-13 14:04:43 -0500

    adds termcolor

--- __main__.py
+++ __main__.py
@@ -4,6 +4,8 @@ import matplotlib as plt
 import plotly.graph_objects as go
 import plotly.io as pio
 
+from termcolor import colored
+
 df = pd.read_csv('911.csv', dtype=dict(
     lat = float,
     lng = float,
@@ -16,12 +18,20 @@ df = pd.read_csv('911.csv', dtype=dict(
     e = str
 ))
 
-print('911 CALLS INFO')
+print(colored('911 CALLS INFO', color='red'))
 print(df.info())
+print()
 
-print('911 CALLS HEAD')
+print(colored('911 CALLS HEAD', color='red'))
 print(df.head())
+print()
 
-print('What are the top 5 zipcodes for 911 calls?')
+print(colored('What are the top 5 zipcodes for 911 calls?', color='green'))
 top_zipcodes = df.groupby('zip').count().sort_values(ascending = False, by = 'e')
 print(top_zipcodes['e'].head(5))
+print()
+
+print(colored('What are the top 5 townships (twp) for 911 calls?', color='green'))
+top_twp = df.groupby('twp').count().sort_values(ascending = False, by = 'e')
+print(top_twp['e'].head(5))
+print()
